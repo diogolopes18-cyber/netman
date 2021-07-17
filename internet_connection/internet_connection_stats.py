11351d746b9e375095ebf0b03956034a53fb6137
diogolopes18-cyber <diogolopes18@ieee.org>
2021-07-17 17:42:06 +0100

    Change return variable

--- internet_connection/internet_connection_stats.py
+++ internet_connection/internet_connection_stats.py
@@ -49,7 +49,7 @@ def connection_speed(*args, **kwargs):
         "ping": ping
     }
 
-    print(json.dumps(results, indent=2))
+    return results
 
 if __name__ == "__main__":
     connection_speed()

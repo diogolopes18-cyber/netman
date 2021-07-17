819438fd4fa7e3d9c403e30481bd8bbd1fb75aee
diogolopes18-cyber <diogolopes18@ieee.org>
2021-07-17 15:47:33 +0100

    (fix): add comma

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        license = "MIT"
+        license = "MIT",
         install_requires=[], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
         
